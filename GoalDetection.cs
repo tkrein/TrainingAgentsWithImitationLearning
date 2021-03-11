561d4d259deeb67fccf72a9d5ec535e6862bdd04
tkrein <tomkrein@web.de>
2021-03-11 12:51:20 +0100

    Add files via upload

---
+++ GoalDetection.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GoalDetection : MonoBehaviour
+{
+    [HideInInspector]
+    public PushAgent agent;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("goal"))
+        {
+            agent.ScoredAGoal();
+        }
+    }
+
+}
